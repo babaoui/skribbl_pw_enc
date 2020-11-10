1b4cff6dce5ee84bbd6b8cb6e535ea79b6702c6a
babaoui <73282716+babaoui@users.noreply.github.com>
2020-11-10 14:54:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ let o = document.getElementById('o');
 let g = document.getElementById('g');
 
 g.addEventListener('click', () => {
+    o.hidden = 0;
     o.innerHTML = i.value.split('_').map(s => s.split('').map(c => String.fromCharCode(c.charCodeAt()-1)).join(''))
+    o.select();
+    document.execCommand("copy");
+    o.hidden = 1;
 });
